a0853f1257d472a6d6c270cfd0a8fa0bbf83493f
Gabe Munoz <gabe.munoz@ibm.com>
2020-07-01 10:09:45 -0400

    initial recipes app - basic folder structure + first components

---
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -0,0 +1,18 @@
+import { Recipe } from './../../recipe.model';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-item',
+  templateUrl: './recipe-item.component.html',
+  styleUrls: ['./recipe-item.component.css']
+})
+export class RecipeItemComponent implements OnInit {
+
+  constructor() { }
+
+  @Input() recipeItem: Recipe;
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"],
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "Risotto",
+      "Creamy and delicious",
+      "https://www.recipetineats.com/wp-content/uploads/2019/10/Mushroom-Risotto_7.jpg"
+    ),
+    new Recipe(
+      "Risotto",
+      "Creamy and delicious",
+      "https://www.recipetineats.com/wp-content/uploads/2019/10/Mushroom-Risotto_7.jpg"
+    ),
+    new Recipe(
+      "Risotto",
+      "Creamy and delicious",
+      "https://www.recipetineats.com/wp-content/uploads/2019/10/Mushroom-Risotto_7.jpg"
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
+}
